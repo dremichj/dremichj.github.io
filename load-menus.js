957083976ea6ae68d68163086ba2139c35a3763f
dremichj <mjdjr2004@gmail.com>
2025-06-29 19:19:45 -0400

    Added reusable header function

---
+++ load-menus.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', () => {
+    fetch('header.html')
+    .then(res =>res.text())
+    .then(html => {
+      document.getElementById('header-placeholder').innerHTML = html;
+    })
+    .catch(err => console.error('Error loading header:',err));
+});
