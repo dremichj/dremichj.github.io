1c6031c417ed3f8db70b0c62e5bd2b3def326e5e
dremichj <mjdjr2004@gmail.com>
2025-06-29 16:13:27 -0400

    Fixed js

--- load-projects.js
+++ load-projects.js
@@ -1,13 +1,15 @@
-fetch('projects.txt')
-  .then(response => {
-    if (!response.ok) {
-      throw new Error('Network response was not ok');
-    }
-    return response.text();
-  })
-  .then(data => {
-    document.getElementById('projects-text').textContent = data;
-  })
-  .catch(error => {
-    console.error('Error fetching the text file:', error);
+document.addEventListener('DOMContentLoaded', () => {
+    fetch('projects.txt')
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.text();
+      })
+      .then(data => {
+        document.getElementById('projects-text').textContent = data;
+      })
+      .catch(error => {
+        console.error('Error fetching the text file:', error);
+      });
   });
