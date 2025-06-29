0cd88d0241b753f1357d04fffa9ca4d600738171
dremichj <mjdjr2004@gmail.com>
2025-06-29 16:06:48 -0400

    Test again

--- load-projects.js
+++ load-projects.js
@@ -1,13 +1,13 @@
 fetch('projects.txt')
-    .then(response=>{
-        if(!response.ok){
-            throw new Error('Network response not ok');
-        }
-        return response.text();
-    })
-    .then(data => {
-        document.getElementById('projects-textx').textContent = data;
-    })
-    .catch(error =>{
-        console.error('Error fetching the text file:',error);
-    });
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    return response.text();
+  })
+  .then(data => {
+    document.getElementById('projects-text').textContent = data;
+  })
+  .catch(error => {
+    console.error('Error fetching the text file:', error);
+  });
