2403cf1e638e8a8c10c3fee69b1ea2139c9e9784
dremichj <mjdjr2004@gmail.com>
2025-06-29 23:14:49 -0400

    Stylizing

--- load-projects.js
+++ load-projects.js
@@ -10,14 +10,24 @@ document.addEventListener('DOMContentLoaded', () => {
   
         let currentTitle = '';
         let currentDesc = '';
+        let currentSkills=[];
+        let currentLink;
   
         lines.forEach(line => {
           line = line.trim();
+          // Gets the title and description
           if (line.startsWith('T:"') && line.endsWith('"')) {
             currentTitle = line.slice(3, -1);
-          } else if (line.startsWith('D:"') && line.endsWith('"')) {
+          } if (line.startsWith('D:"') && line.endsWith('"')) {
             currentDesc = line.slice(3, -1);
-  
+          } if (line.startsWith('S:')){
+            const skills = line.slice(2).trim();
+            currentSkills = skills.split(/\s+/);
+          } else if (line.startsWith('L:')){
+            currentLink = line.slice(2).trim();
+          }
+
+          if (currentTitle && currentDesc && currentSkills.length >0){
             const project = document.createElement('div');
             project.className = 'column-box';
   
@@ -27,14 +37,46 @@ document.addEventListener('DOMContentLoaded', () => {
             const descriptionElement = document.createElement('p');
             descriptionElement.textContent = currentDesc;
   
-            project.appendChild(titleElement);
-            project.appendChild(descriptionElement);
+            const contentWrapper = document.createElement('div');
+            contentWrapper.className='project-content';
+
+            contentWrapper.appendChild(titleElement);
+            contentWrapper.appendChild(descriptionElement);
+            project.appendChild(contentWrapper)
+
+            const skillContainer = document.createElement('div');
+            skillContainer.className = 'skill-container';
+
+            currentSkills.forEach(skill=> {
+              const skillBox = document.createElement('div');
+              skillBox.className = 'skillbox';
+              skillBox.textContent=skill;
+              skillContainer.appendChild(skillBox);
+            });
+
+            const linkBtn = document.createElement('a');
+            linkBtn.className='project-link';
+            linkBtn.href = currentLink;
+
+            const img = document.createElement('img');
+            img.src='github-mark-white.png';
+            img.className = 'github-link-logo';
+            linkBtn.appendChild(img);
+
+            skillContainer.appendChild(linkBtn);
+
+
+            project.appendChild(skillContainer);
+
+
             container.prepend(project);
-  
             // Reset for next project
             currentTitle = '';
             currentDesc = '';
+            currentSkills=[];
+            currentLink='';
           }
+          
         });
       })
       .catch(err => console.error('Error loading projects:', err));
